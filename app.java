e25d6f35f831d1924c175fd17e85c4dcfb3a72ab
sampathchoudary <sampathkumar4695@gmail.com>
2026-04-03 08:06:08 +0530

    dev 2 added code

--- app.java
+++ app.java
@@ -6,3 +6,17 @@ public class AddTwoIntegers {
        System.out.println("The sum is: " + sum);
    }
 }
+
+#dev2 added code
+   import java.util.Scanner;
+public class CheckEvenOdd {
+   public static void main(String[] args) {
+       Scanner reader = new Scanner(System.in);
+       System.out.print("Enter a number: ");
+       int num = reader.nextInt();
+       if (num % 2 == 0)
+           System.out.println(num + " is even");
+       else
+           System.out.println(num + " is odd");
+   }
+}
